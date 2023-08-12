6710bc9258b2edc8ceaec102244de3d6dcd8d10c
Aron <aron.cachago@gmail.com>
2023-08-11 22:25:15 -0500

    logica en un arreglo

--- Academico/views.py
+++ Academico/views.py
@@ -1,42 +1,62 @@
 from django.shortcuts import render , redirect
-from .models import Curso
 # Create your views here.
 
+cursos = [
+    {'codigo': 'C001', 'nombre': 'Desarrollo en Python', 'credito': '3'},
+    {'codigo': 'C002', 'nombre': 'Desarrollo en Java', 'credito': '4'},
+    {'codigo': 'C003', 'nombre': 'Desarrollo en Angular', 'credito': '2'},
+    {'codigo': 'C004', 'nombre': 'Frontend con React', 'credito': '3'},
+    {'codigo': 'C005', 'nombre': 'Backend con Spring Boot', 'credito': '5'},
+] 
+
 def home(request):
-    cursos = Curso.objects.all()
-    return render(request,"gestionCursos.html" , {"cursos":cursos})
+    return render(request, "gestionCursos.html", {"cursos": cursos})
 
 def registrarCurso(request):
-    codigo= request.POST['txtCodigo']
-    nombre= request.POST['txtNombre']
-    credito= request.POST['txtCreditos']
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    credito = request.POST['txtCreditos']
 
-    curso = Curso.objects.create(
-        codigo = codigo,
-        nombre = nombre,
-        credito = credito
-    )
+    curso = {
+        'codigo': codigo,
+        'nombre': nombre,
+        'credito': credito
+    }
+
+    cursos.append(curso)
 
     return redirect('/')
 
-def eliminarCurso(request , codigo):
-    curso = Curso.objects.get(codigo = codigo)
-    curso.delete()
+def eliminarCurso(request, codigo):
+    curso_index = None
+    for index, curso in enumerate(cursos):
+        if curso['codigo'] == codigo:
+            curso_index = index
+            break
+
+    if curso_index is not None:
+        cursos.pop(curso_index)
 
     return redirect('/')
 
 def editarCurso(request, codigo):
-    curso = Curso.objects.get(codigo = codigo)
-    return render(request , "gestionCursos.html" , {"editarCurso":curso})
+    curso = None
+    for c in cursos:
+        if c['codigo'] == codigo:
+            curso = c
+            break
+
+    return render(request, "gestionCursos.html", {"editarCurso": curso})
 
 def edicionCurso(request):
-    codigo= request.POST['txtCodigo']
-    nombre= request.POST['txtNombre']
-    credito= request.POST['txtCreditos']
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    credito = request.POST['txtCreditos']
 
-    curso = Curso.objects.get(codigo = codigo)
-    curso.nombre = nombre
-    curso.credito = credito
-    curso.save()
+    for curso in cursos:
+        if curso['codigo'] == codigo:
+            curso['nombre'] = nombre
+            curso['credito'] = credito
+            break
 
     return redirect('/')
